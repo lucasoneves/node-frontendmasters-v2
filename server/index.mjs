f9b3bbad853ef9db2560e08224ea8107b93a67aa
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-03-01 16:43:34 -0300

    Creating a low-level server

---
+++ server/index.mjs
@@ -0,0 +1,32 @@
+import http from 'http'
+
+const host = 'localhost'
+const port = 8000
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST') {
+    let body = ''
+
+    req.on('data', chunk => {
+      body += chunk.toString()
+    })
+
+    req.on('end', () => {
+      if (req.headers['content-type'] === 'application/json') {
+        body = JSON.parse(body)
+      }
+
+      console.log(body)
+      res.writeHead(201)
+      res.end('ok')
+    })
+  } else {
+    res.writeHead(200)
+    res.end('hello from my server')
+  }
+
+})
+
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`)
+})
