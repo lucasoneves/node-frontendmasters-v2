2e6bb389be4da111c696c19825cf828bed2407d5
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-03-02 19:23:58 -0300

    Server api with express

---
+++ server/server.mjs
@@ -0,0 +1,32 @@
+import bodyParser from 'body-parser'
+import express from 'express'
+import morgan from 'morgan'
+
+const app = express()
+
+const port = 8000
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+
+const db = []
+
+app.post('/todo', (req, res, next) => {
+  const data = {
+    id: Date.now(),
+    text: req.body.text
+  }
+
+  db.push(data)
+  res.status(201)
+  res.json(data)
+})
+
+app.get('/todo', (req, res, next) => {
+  res.json(db);
+})
+
+app.listen(port, () => {
+  console.log(`Server on http://localhost:${port}`)
+})
